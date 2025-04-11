b634489f9e433ae38dbcdac5fb2c9fe52147c82c
chrnnn <ijhok1234@naver.com>
2025-04-11 09:55:13 +0900

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
-git add .
-git commit -m "updated"
-git push
+# 파일이 바뀌었을 때만 커밋
+if git diff --quiet && git diff --cached --quiet; then
+  echo "Nothing to commit."
+else
+  echo "Enter commit message: "
+  read msg
+  git add .
+  git commit -m "$msg"
+  git push
+fi
